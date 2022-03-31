9c8f380c0c4574c67c0f8e1b327339fe00386a9a
landeholt <johnlan@kth.se>
2022-03-31 19:51:00 +0200

    change conditions

--- dd2482_course_automation/main.py
+++ dd2482_course_automation/main.py
@@ -218,7 +218,7 @@ def give_feedback(payload: Payload, secret: Optional[str], error_message: Option
             repos.append("No repos found..")  # type: ignore
         created_at = result["created_at"]
         decision_message = "\n---\n\nDecision is based on the following findings:\n\n"
-        decision_message += f'assumed stage: `{stage}`'
+        decision_message += f'assumed stage: `{stage}`\n'
         decision_message += f"created_at: {created_at}\n"
         decision_message += f"repos:\n"
         decision_message += '\n'.join(map(lambda x : '\t- ' + x,repos))
